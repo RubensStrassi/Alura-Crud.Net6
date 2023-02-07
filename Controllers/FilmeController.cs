bc7520d82d3e06de0a40843592465b5bc261a250
RubensStrassi <rubensstrassi@gmail.com>
2023-02-07 17:23:46 -0300

    Primeira parte

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,34 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController: ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+
+    [HttpPost]
+    public IActionResult AdcionaFilme([FromBody]Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+        return CreatedAtAction(nameof(RecupearFilmePorId), new {id = filme.Id}, filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilme([FromQuery] int skip = 0, [FromQuery] int take = 10)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecupearFilmePorId(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Titulo Obrigatioro")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "Genero Obrigatioro")]
+    [MaxLength(50, ErrorMessage = "Tem que ser menos que 50")]
+    public string Genero { get; set; }
+
+    [Required]
+    [Range(70,600, ErrorMessage = "Duração de 70 a 600")]
+    public int Duracao { get; set; }
+   
+}
